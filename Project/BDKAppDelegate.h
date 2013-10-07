460d11562929f245ee09b42c9b8eaf13b5393395
Ben Kreeger <ben@kree.gr>
2013-10-06 20:22:41 -0500

    Adding project files.

---
+++ Project/BDKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BDKAppDelegate.h
+//  BDKSlider
+//
+//  Created by Ben Kreeger on 10/6/13.
+//  Copyright (c) 2013 Ben Kreeger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
